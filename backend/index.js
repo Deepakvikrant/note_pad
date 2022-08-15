389e881a2ab484c4b5b67a1c37a785bcc5a7a56b
Deepakvikrant <100572841+Deepakvikrant@users.noreply.github.com>
2022-08-15 12:59:11 +0530

    connection statblished

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ const express = require('express')
 
 connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World harryyy l dddd !')
-})
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
