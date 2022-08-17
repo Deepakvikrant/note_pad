4d2bca5ceda80ef55c429531c08eb5d56ef54271
Deepakvikrant <100572841+Deepakvikrant@users.noreply.github.com>
2022-08-17 10:14:57 +0530

    get user, login are created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const fetchuser = require('../middleware/fetchuser');
 const router = express.Router();
+const notes = require('../models/notes');
 
-router.get('/', (req, res)=>{
+//routes 1: get all the notes of the user #login required
+router.get('/fetchallnotes',fetchuser, (req, res)=>{
     
     res.json([])
 } )
