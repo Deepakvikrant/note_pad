f6a730e4a2034651af5935d9c3d6257e09abb531
Deepakvikrant <100572841+Deepakvikrant@users.noreply.github.com>
2022-08-23 21:38:54 +0530

    Frontend validation are added

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -7,11 +7,12 @@ const AddNote = () => {
     const context = useContext(NoteContext)
     const {addNote} = context;
 
-    const [note,setNote] = useState({title:"",description :"",tag:"default"})
+    const [note,setNote] = useState({title:"",description :"",tag:""})
 
     const handleClick = (e)=>{
         e.preventDefault();
     addNote(note.title,note.description,note.tag);
+    setNote({title:"",description:"",tag:""});
     }
 
     const onChange=(e)=>{
@@ -25,17 +26,17 @@ const AddNote = () => {
             <form className='my-3'>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
-                    <input type="text" onChange={onChange} className="form-control" id="title" name="title" aria-describedby="emailHelp" />
+                    <input minLength={5} type="text" onChange={onChange} value={note.title} className="form-control" id="title" name="title" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="description" className="form-label">Description</label>
-                    <input type="text" onChange={onChange} className="form-control" id="description" name='description' />
+                    <input minLength={5} required type="text" onChange={onChange} value={note.description} className="form-control" id="description" name='description' />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="tag" className="form-label">Tag</label>
-                    <input type="text" onChange={onChange} className="form-control" id="tag" name='tag' />
+                    <input type="text" onChange={onChange} value={note.tag} className="form-control" id="tag" name='tag' />
                 </div>
-                <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Notes</button>
+                <button type="submit" disabled={note.title.length <5 || note.description.length < 5} className="btn btn-primary" onClick={handleClick}>Add Notes</button>
             </form>
 
 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -53,11 +53,11 @@ const refClose = useRef(null)
               <form className="my-3">
                 <div className="mb-3">
                   <label htmlFor="title" className="form-label">Title</label>
-                  <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} />
+                  <input type="text" minLength={5} required className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="description" className="form-label">Description</label>
-                  <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} />
+                  <input type="text" minLength={5} required className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="tag" className="form-label">Tag</label>
@@ -68,7 +68,7 @@ const refClose = useRef(null)
             </div>
             <div className="modal-footer">
               <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
+              <button disabled={note.etitle.length < 5 || note.edescription.length < 5} onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
             </div>
           </div>
         </div>
